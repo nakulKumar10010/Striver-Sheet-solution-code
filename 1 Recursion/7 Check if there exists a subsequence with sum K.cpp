becd992d4905c8e6d29c5445d68798af45a7a4c8
nakulKumar10010 <155071888+nakulKumar10010@users.noreply.github.com>
2025-05-30 19:13:15 +0530

    Create 7 Check if there exists a subsequence with sum K.cpp

---
+++ 1 Recursion/7 Check if there exists a subsequence with sum K.cpp
@@ -0,0 +1,29 @@
+void solve(int n,int i, vector<int>& arr,int& currSum, int k, bool& flag){
+    //base case
+    if(flag) return;
+    if(i >= n){
+        if(k == currSum){
+            flag = true;
+        }
+        
+        return;
+    }
+    //take
+    currSum =currSum + arr[i];
+    solve(n, i+1, arr, currSum, k, flag);
+    //backtrack
+    currSum = currSum-arr[i];
+    
+    //not take 
+    solve(n, i+1, arr, currSum, k, flag);
+    
+}
+
+int findWays(vector<int>& arr, int k)
+{
+	bool flag = false;
+    int currSum = 0;
+	solve(arr.size(), 0, arr, currSum, k, flag);
+
+	return count;
+}
