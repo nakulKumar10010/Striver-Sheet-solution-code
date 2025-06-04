e7c10c8f74c4f8a15a1900ff2bdf7dd1aab1cf51
nakulKumar10010 <155071888+nakulKumar10010@users.noreply.github.com>
2025-06-05 00:40:55 +0530

    Create 16 N Queen- optimised.cpp

---
+++ 1 Recursion/16 N Queen- optimised.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans,
+               int n, vector<int>& leftRow, vector<int>& upperDiagonal,
+               vector<int>& lowerDiagonal) {
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            // check placed
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 &&
+                upperDiagonal[n - 1 + col - row] == 0) {
+                // palced and marked placed
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+                // recursive call
+                solve(col + 1, board, ans, n, leftRow, upperDiagonal,
+                      lowerDiagonal);
+                // backtrack
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for (int i = 0; i < n; i++) {
+            board[i] = s;
+        }
+        vector<int> leftRow(n, 0), upperDiagonal(2 * n - 1, 0),
+            lowerDiagonal(2 * n - 1, 0);
+        solve(0, board, ans, n, leftRow, upperDiagonal, lowerDiagonal);
+        return ans;
+    }
+};
